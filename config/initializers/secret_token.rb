45f0bc611385c67a127e3a80b064707266fee40d
Chris Tosswill <ctosswill@engineyard.com>
2011-07-21 13:21:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobapp::Application.config.secret_token = 'd7ba10d3b66083f288fc434cd1a3ace1655bf814f1863cf697a21617bb6c831a693d3bb2199aeba3b5d6bb7014d104eb8766afe0772ffacf87012f2607d79d01'
